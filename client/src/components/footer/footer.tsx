54618795757727d2fa1cacd027fb1ee0894c941c
bryceberczik <berczikbryce@gmail.com>
2024-09-16 16:44:21 -0500

    added header with login and search icons, with a brand name

---
+++ client/src/components/footer/footer.tsx
@@ -0,0 +1,13 @@
+import './footer.css'
+
+export default function Footer() {
+
+    return(
+
+        <div>
+            <footer>
+                <h3 className='footer-text'>© 2024 Trip Zen. All rights reserved.</h3>
+            </footer>
+        </div>
+    )
+}
