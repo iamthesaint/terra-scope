a1015ec37b634913670478b0809ade8b9911ab11
iamthesaint <stephenie2@me.com>
2024-09-17 18:40:56 -0400

    added spin, need to integrate outlet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Outlet } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import Home from "./pages/Home";
 import './App.css';
 
 const App = () => {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Routes>
-          // <Route path="/" element={<Home />} />
-        </Routes>
-        <main>
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-    // </Router>
+ return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
   );
-};
+}
 
 export default App;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,7 +4,6 @@ import Map from '../components/Map';
 const Home: React.FC = () => {
 return (
 <div>
-<h1>Home</h1>
 <Map />
 </div>
 );
