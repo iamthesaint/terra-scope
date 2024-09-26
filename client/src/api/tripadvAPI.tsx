1edcfafd055d0136c2b1b39941a2b7db41d7dde2
iamthesaint <stephenie2@me.com>
2024-09-26 19:46:34 -0400

    deploy2

--- client/src/api/tripadvAPI.tsx
+++ client/src/api/tripadvAPI.tsx
@@ -1,7 +1,7 @@
 export const fetchTripAdvisorData = async (locationId: string) => {
   console.log("Fetching TripAdvisor data for location ID:", locationId);
   try {
-    const apiKey = process.env.VITE_TRIPADVISOR_API_KEY;
+    const apiKey = import.meta.env.VITE_TRIPADVISOR_API_KEY;
     if (!apiKey) {
       throw new Error('TripAdvisor API key is missing');
     }
@@ -10,7 +10,7 @@ export const fetchTripAdvisorData = async (locationId: string) => {
       method: "GET",
       headers: {
         "accept": "application/json",
-        "X-TripAdvisor-API-Key": apiKey,
+        "x-tap-api-key": apiKey,
       }
     });
 
